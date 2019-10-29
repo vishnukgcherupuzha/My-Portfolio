9bed0e135e2f083ee94023305f1c2380104f8fb3
vishnukgcherupuzha@gmail.com <vishnukgcherupuzha@gmail.com>
2019-10-29 22:50:57 +0530

    Updates

---
+++ precache-manifest.cc543cd8f414cd5c274128e449271015.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748aa98d593a061aed1c92d78302d376",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "70067906323043ca5e34",
+    "url": "/My-Portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "989d270336d0e3c65950",
+    "url": "/My-Portfolio/static/css/main.f4d059fc.chunk.css"
+  },
+  {
+    "revision": "70067906323043ca5e34",
+    "url": "/My-Portfolio/static/js/2.ff0f118e.chunk.js"
+  },
+  {
+    "revision": "989d270336d0e3c65950",
+    "url": "/My-Portfolio/static/js/main.1a574272.chunk.js"
+  },
+  {
+    "revision": "746aabeaac602faec95a",
+    "url": "/My-Portfolio/static/js/runtime-main.5c342c6f.js"
+  },
+  {
+    "revision": "af890aa07baf93ffa55b99bd90a59077",
+    "url": "/My-Portfolio/static/media/header3.af890aa0.png"
+  },
+  {
+    "revision": "a844d32cbd4d04223db13d397a53fd7d",
+    "url": "/My-Portfolio/static/media/project1.a844d32c.jpg"
+  },
+  {
+    "revision": "a4213d26d65e80de4777606beee0dc31",
+    "url": "/My-Portfolio/static/media/project2.a4213d26.jpg"
+  },
+  {
+    "revision": "64e3e89ce182aae41ed71b9f259487ee",
+    "url": "/My-Portfolio/static/media/project3.64e3e89c.jpg"
+  },
+  {
+    "revision": "43ab2879eae4a7c02426c9686f15dfbb",
+    "url": "/My-Portfolio/static/media/project4.43ab2879.JPG"
+  },
+  {
+    "revision": "f682e9eaf21f53fabda7d69201d5e522",
+    "url": "/My-Portfolio/static/media/project5.f682e9ea.JPG"
+  }
+]);
