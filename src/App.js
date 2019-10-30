21804c84e87f8592ef48e2888ecd2abf417fd1f0
vishnukgcherupuzha@gmail.com <vishnukgcherupuzha@gmail.com>
2019-10-30 08:23:47 +0530

    browserrouter update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'react-router-dom'
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import {HashRouter,Switch,Route} from 'react-router-dom'
 import './App.css';
 import Resume from './components/Resume'
 import Contactme from './components/Contactme'
@@ -8,13 +8,13 @@ import Default from './components/Default'
 function App() {
   return (
     <React.Fragment>
-    <BrowserRouter>
+    <HashRouter >
         <Switch>
             <Route exact path="/" component={Resume}></Route>
             <Route path="/contactme" component={Contactme}></Route>
             <Route component={Default}></Route>
         </Switch> 
-        </BrowserRouter>   
+        </HashRouter>   
     </React.Fragment>
   );
 }
